41b5856bbabaeb6869183132f86447ad28b8ce8f
kana112233 <makey22112@gmail.com>
2021-10-12 22:41:53 +0800

    close: #61

--- src/main/java/com/baomidou/plugin/idea/mybatisx/codegenerator/utils/MybatisConst.java
+++ src/main/java/com/baomidou/plugin/idea/mybatisx/codegenerator/utils/MybatisConst.java
@@ -17,6 +17,8 @@ public interface MybatisConst {
         new IdTypeObj(IdType.AUTO, "AUTO(ID自增)"),
         new IdTypeObj(IdType.NONE, "NONE"),
         new IdTypeObj(IdType.INPUT, "INPUT(用户输入ID)"),
+        new IdTypeObj(IdType.ASSIGN_ID, "分配ID(主键类型为number或string）"),
+        new IdTypeObj(IdType.ASSIGN_UUID, "分配UUID (主键类型为 string)"),
         new IdTypeObj(IdType.ID_WORKER, "ID_WORKER(全局唯一ID)"),
         new IdTypeObj(IdType.UUID, "UUID(全局唯一ID )"),
         new IdTypeObj(IdType.ID_WORKER_STR, "ID_WORKER_STR(字符串全局唯一ID)")
